a4fd7e6c7c832c1c924cd299055b309028b53f4c
baibhavatsa <baibhavvatsa@gmail.com>
2019-02-25 00:04:14 -0600

    trying to make a list of things i use

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!usr/bin/env/bash
+
+# python
+# entr
+# cipher and stuff
+# stegsolve and stuff?
+# vscode
+# gitkraken
+# normal code
+# slack
+# pull repos if called with arguments
+# vlc
+# vim
+# discord
+# vmware player
+# wireshark
+# tixati
+# IDEs?
+# search for more tools you use not in gtfobins
+# chrome?
+# tor
+# setup wallpaper
+# setup icons and shit
+# opencv
+# ros
+# mathematica?
+# spotify
+# node js
+# npm
+# go
+# prolog
+# dart
+# tensorflow
+# matplotlib
+# numpy
+# beautifulsoup64
+# ida
+# chromium?
+# radare2
+# 
